1f22be3cfc04d953e9037f13a9b05a1b761e7341
Jhon Edinson Castañeda Oviedo <48686871+JhonEdinson-Shongo@users.noreply.github.com>
2022-03-26 12:36:38 -0500

    ajuste video

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -24,7 +24,6 @@
     figure.mb-5
       .video
         iframe(width="560" height="315" src="https://www.youtube.com/embed/IO9IGwTniw8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-      figcaption Video: Leyenda del video
     
 
 </template>
